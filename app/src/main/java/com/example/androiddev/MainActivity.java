2a739ec6fa52643dcafee3c5ae06c4b8563f78e5
mahfo <mahfoudhoumad2004@gmail.com>
2026-02-03 17:07:12 +0100

    Initial commit

---
+++ app/src/main/java/com/example/androiddev/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.androiddev;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    TextView t;
+    TextInputEditText t1;
+    TextInputEditText t2;
+    TextInputEditText t3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        t1 = findViewById(R.id.t1) ;
+        t2 = findViewById(R.id.t2) ;
+        t3 = findViewById(R.id.t3) ;
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = String.valueOf(t1.getText());
+                t2.setText(text);
+                t3.setText(String.valueOf((text.length())));
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
